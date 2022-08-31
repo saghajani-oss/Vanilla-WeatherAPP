a465b7f2f32dbb3380f727eadf0d094d53995678
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2022-08-31 17:01:54 +0430

    current weather JS and some changes in HTML & CSS

---
+++ Js/app.js
@@ -0,0 +1,25 @@
+function CurrentWeather(response) {
+    console.log(response);
+
+    let temperatureElement = document.querySelector("#currentweather");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    let descriptionElement = document.querySelector(".weather-Condition");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+
+    
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
+
+
+let apiKey = "3bad7bd4137a5ff9f5c84125992a313a";
+let city= "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(CurrentWeather);
