1d6bcc25bd8e1119d04a4a7c42c51e52b31cf1b9
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2022-09-01 10:41:40 +0430

    Code for Time&Date

--- Js/app.js
+++ Js/app.js
@@ -1,4 +1,24 @@
+function showDateTime (timetemp){
+    let now = new Date(timetemp);
+    let hours = now.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = now.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[now.getDay()];
+    return `${day} ${hours}:${minutes}`
+}
+
+
+
+
 function CurrentWeather(response) {
+
+    console.log(response);
 
     let temperatureElement = document.querySelector("#currentweather");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -12,6 +32,10 @@ function CurrentWeather(response) {
     
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+
+    let dateTimeElement = document.querySelector("#date-Time");
+    dateTimeElement.innerHTML = showDateTime(response.data.dt);
+    
 
 }
 
