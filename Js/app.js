9cc7b12f82ee8d1a75bc717d11a4c181f070ab93
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2022-09-01 13:18:02 +0430

    setting weather Icon

--- Js/app.js
+++ Js/app.js
@@ -1,48 +1,52 @@
-function showDateTime (timetemp){
-    let now = new Date(timetemp);
-    let hours = now.getHours();
-    if (hours < 10) {
-        hours = `0${hours}`;
-    }
-    let minutes = now.getMinutes();
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = days[now.getDay()];
-    return `${day} ${hours}:${minutes}`
+function showDateTime(timetemp) {
+  let now = new Date(timetemp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-
-
-
 
 function CurrentWeather(response) {
+  let temperatureElement = document.querySelector("#currentweather");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
-    console.log(response);
+  let descriptionElement = document.querySelector(".weather-Condition");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 
-    let temperatureElement = document.querySelector("#currentweather");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
 
-    let descriptionElement = document.querySelector(".weather-Condition");
-    descriptionElement.innerHTML = response.data.weather[0].description;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
+  let dateTimeElement = document.querySelector("#date-Time");
+  dateTimeElement.innerHTML = showDateTime(response.data.dt * 1000);
 
-    
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-
-    let dateTimeElement = document.querySelector("#date-Time");
-    dateTimeElement.innerHTML = showDateTime(response.data.dt * 1000);
-    
-
+  let iconElement = document.querySelector(".weather-img");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
-
 let apiKey = "3bad7bd4137a5ff9f5c84125992a313a";
-let city= "New York";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(CurrentWeather);
